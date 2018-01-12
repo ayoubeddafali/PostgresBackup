c449aca5684909f2b552686596504d7a71a6a475
ayoubensalem <ayoubensalem@gmail.com>
2018-01-12 17:26:51 +0000

    adding entry_points

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
 
 
-with open("README.rst", "r") as f: 
+with open("README.rst", "r") as f:
 	readme = f.read()
 
 setup(
-	name="pgbackup", 
-	version="0.1.0", 
+	name="pgbackup",
+	version="0.1.0",
 	description="Database backups",
-	long_description=readme, 
+	long_description=readme,
 	author='Ayoubensalem',
 	author_email='ayoubensalem@gmail.com',
-	packages=find_packages('src'), 
+	packages=find_packages('src'),
 	package_dir={'':'src'},
-	install_requires=[]
-) 
+	install_requires=[],
+    entry_points={
+            'console_scripts': [
+                'pgbackup=pgbackup.cli:main'],
+        }
+)
