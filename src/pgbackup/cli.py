827acccd82151fbad2e23672f46f8534f3017545
ayoubensalem <ayoubensalem@gmail.com>
2018-01-12 15:25:41 +0000

    refactoring code to meet tests

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,8 +1,11 @@
 import argparse
 
 class DriverAction(argparse.Action):
+    known_drivers = ['s3', 'local']
     def __call__(self, parser, namespace, values, option_string=None):
         driver, destination = values
+        if driver.lower() not in self.known_drivers:
+            parser.error("Unknown driver")
         namespace.driver = driver.lower()
         namespace.destination = destination
 
@@ -16,8 +19,14 @@ def create_parser():
     parser.add_argument('url', help='URL of database to backup')
     parser.add_argument('--driver', help='How & where to store backtup',
             nargs=2,
+            metavar=("DRIVER", "DESTINATION"),
             action=DriverAction,
             required=True)
     return parser
+
+
+if __name__== "__main__":
+    parser = create_parser()
+    parser.parse_args()
 
 
